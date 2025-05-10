67712a79e4a046b3f192e291441a88073c312f57
João Gabriel Lima Marinho <jg.limamarinho202@gmail.com>
2025-05-09 21:52:08 -0300

    first commit

---
+++ src/main/java/com/jglmarinho/rtp/streaming/RtpStreaming.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.jglmarinho.rtp.streaming;
+
+/**
+ *
+ * @author marinho
+ */
+public class RtpStreaming {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
